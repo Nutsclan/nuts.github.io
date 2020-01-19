213ffff8b1069cc1ad7d16d12e4368bd9d5cd712
Nutsclan <60054341+Nutsclan@users.noreply.github.com>
2020-01-19 20:57:14 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,5 @@
+let DATA = {
+    The_Fringes: [
+        {level: '60', hunt: true, name: '례쉬', location: '기라바니아 변방지대', coordinate: '( 11.0, 13.6 )', drop: '<a href=\'http://ff14.inven.co.kr/dataninfo/item/detail.php?code=19930\' id=\'drop\' target=\'_blank\'>너도밤나무 원목</a>'},
+    ]
+};
